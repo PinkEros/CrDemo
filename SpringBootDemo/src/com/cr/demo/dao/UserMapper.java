63e15fa097e3efc1f9944fef5a33d1ab9ad8bc66
6eek <35479984+PinkEros@users.noreply.github.com>
2018-10-29 16:35:07 +0800

    SpringBoot-Mybatis-Generator基础框架

---
+++ SpringBootDemo/src/com/cr/demo/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.cr.demo.dao;
+
+import main.java.com.cr.demo.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
